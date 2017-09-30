0472e672f620b22b12806322d750635af3f0e9e0
Micah Wood <micah@wpscholar.com>
2017-09-30 15:43:44 -0400

    Initial commit.

---
+++ PostsQuery.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace wpscholar\WordPress;
+
+/**
+ * Class PostsQuery
+ *
+ * @package wpscholar\WordPress
+ */
+class PostsQuery implements \Countable, \IteratorAggregate {
+
+	/**
+	 * Cache expiration, in seconds
+	 *
+	 * @var int
+	 */
+	public $cache_expiration = MINUTE_IN_SECONDS;
+
+	/**
+	 * Properties of the WP_Query object to retain in the cache
+	 *
+	 * @see https://pressjitsu.com/blog/dont-cache-wp_query/
+	 *
+	 * @var array
+	 */
+	public $cached_properties = [
+		'found_posts',
+		'max_num_pages',
+		'post_count',
+		'posts',
+	];
+
+	/**
+	 * @var \WP_Query
+	 */
+	public $query;
+
+	/**
+	 * Fetch query results
+	 *
+	 * @param array|string $args
+	 *
+	 * @return \WP_Query|\stdClass Returns WP_Query instance or a pseudo WP_Query object
+	 */
+	public function fetch( $args = [] ) {
+
+		$query_args = wp_parse_args( $args );
+
+		ksort( $query_args );
+
+		$cache_key = md5( __METHOD__ . '?' . http_build_query( $query_args, null, '&' ) );
+
+		$query = wp_cache_get( $cache_key );
+
+		if ( ! $query ) {
+			$query = new \WP_Query();
+			$query->query( $query_args );
+
+			$pseudoQuery = [];
+			foreach ( $this->cached_properties as $property ) {
+				$pseudoQuery[ $property ] = $query->$property;
+			}
+
+			wp_cache_set( $cache_key, (object) $pseudoQuery, '', $this->cache_expiration );
+		}
+
+		$this->query = $query;
+
+		return $query;
+	}
+
+	/**
+	 * Required method for IteratorAggregate interface.
+	 *
+	 * @return \Generator
+	 */
+	public function getIterator() {
+		try {
+			if ( isset( $this->query, $this->query->posts ) ) {
+				foreach ( $this->query->posts as $post ) {
+					setup_postdata( $post );
+					yield $post;
+				}
+			}
+		} finally { // Once loop is done, or we break out
+			wp_reset_postdata();
+		}
+	}
+
+	/**
+	 * Required method for Countable interface.
+	 *
+	 * @return int
+	 */
+	public function count() {
+		return isset( $this->query, $this->query->posts ) ? count( $this->query->posts ) : 0;
+	}
+
+}
